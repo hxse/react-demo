f156ccfbad5f11a73ce1f979fd072e7e9c8a4e01
hxse <narutohy@163.com>
2022-01-11 20:14:01 +0800

    change readme

--- push react-demo.bat
+++ push react-demo.bat
@@ -2,5 +2,5 @@ chcp 65001
 set /p c=请输入commit:
 git add .
 git commit -m "%c%"
-git push https://hxse:%hxse_github_token%@github.com/hxse/react-demo.git
+git push https://hxse:%hxse_github_token%@github.com/hxse/react-hook-update.git
 pause
